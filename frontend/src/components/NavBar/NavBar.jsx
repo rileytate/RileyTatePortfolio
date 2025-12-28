10c96c9b5b65337a4bced6ba00c5e82dbc0d9c9d
Riley Tate <tater02@pfw.edu>
2025-12-28 15:51:26 -0500

    editing links in navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -6,19 +6,19 @@ function NavBar () {
     return (
         <>
             <nav>
-                <Link to={'/'} className={styles.link}>
+                <Link to={'/RileyTatePortfolio/'} className={styles.link}>
                     <h2>Home</h2>
                 </Link>
 
-                <Link to={'/AboutMe'} className={styles.link}>
+                <Link to={'/RileyTatePortfolio/AboutMe'} className={styles.link}>
                     <h2>About Me</h2>
                 </Link>
 
-                <Link to={'/Projects'} className={styles.link}>
+                <Link to={'/RileyTatePortfolio/Projects'} className={styles.link}>
                     <h2>Projects</h2>
                 </Link>
 
-                <Link to={'/Experiences'} className={styles.link}>
+                <Link to={'/RileyTatePortfolio/Experiences'} className={styles.link}>
                     <h2>Experiences</h2>
                 </Link>
             </nav>
